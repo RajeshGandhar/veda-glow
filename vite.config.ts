12664a819479cc3dd71532c98552478d4522332c
RajeshGandhar <rajeshgandhar12345@gmail.com>
2026-04-22 12:31:59 +0530

    impove ments

--- vite.config.ts
+++ vite.config.ts
@@ -33,12 +33,5 @@ export default defineConfig({
         },
       },
     },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 });
